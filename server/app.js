f409273088ff3b677eef6604587fd555e32ac669
Rashika Shah <rashikashah0310@gmail.com>
2026-08-08 19:56:56 +0530

    feat: connect MongoDB and add User model

--- server/app.js
+++ server/app.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Zynvora API is running');
+app.get("/", (req, res) => {
+  res.send("Collavix API is running");
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
